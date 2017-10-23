3efda6aa3722185cbdcd5edef183d0cc306e3dab
Thomas Boerger <thomas@webhippie.de>
2017-10-23 13:11:50 +0200

    Fixed directory naming

--- rootfs/etc/s6/php/custom.d/10-copy.sh
+++ rootfs/etc/s6/php/custom.d/10-copy.sh
@@ -4,9 +4,11 @@ if [[ -d /srv/www/plugins ]]
 then
   for PLUGIN in $(find /srv/www/plugins -type d -maxdepth 1 -mindepth 1)
   do
-    echo "> copying ${PLUGIN} plugin"
-    rm -rf /var/lib/piwik/plugins/${PLUGIN}
-    cp -rf /srv/www/plugins/${PLUGIN} /var/lib/piwik/plugins/
+    NAME=$(basename ${PLUGIN})
+
+    echo "> copying ${NAME} plugin"
+    rm -rf /var/lib/piwik/plugins/${NAME}
+    cp -rf /srv/www/plugins/${NAME} /var/lib/piwik/plugins/
   done
 fi
 
