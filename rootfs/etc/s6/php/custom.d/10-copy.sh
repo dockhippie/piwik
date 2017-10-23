1ca2104ee7784e96dbee552bf66d7c9aadb5bc2d
Thomas Boerger <thomas@webhippie.de>
2017-10-23 13:05:35 +0200

    Fixed typo while plugin copy

--- rootfs/etc/s6/php/custom.d/10-copy.sh
+++ rootfs/etc/s6/php/custom.d/10-copy.sh
@@ -2,7 +2,7 @@
 
 if [[ -d /srv/www/plugins ]]
 then
-  for PLUGIN in $(find /var/lib/piwik/plugins -type d -maxdepth 1 -mindepth 1)
+  for PLUGIN in $(find /srv/www/plugins -type d -maxdepth 1 -mindepth 1)
   do
     echo "> copying ${PLUGIN} plugin"
     rm -rf /var/lib/piwik/plugins/${PLUGIN}
