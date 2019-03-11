67b62697b92818e3b1f5e085dfcebdbdf9f8f400
Thomas Boerger <thomas@webhippie.de>
2019-03-11 16:17:34 +0100

    Migrate to drone pipeline

--- latest/overlay/etc/s6/php/custom.d/15-symlink.sh
+++ latest/overlay/etc/s6/php/custom.d/15-symlink.sh
@@ -4,16 +4,16 @@ echo "> symlink app plugins"
 rm -rf /srv/www/plugins
 ln -sf \
   /var/lib/piwik/plugins \
-  /srv/www/plugins
+  ${CADDY_WEBROOT}/plugins
 
 echo "> symlink app config"
 rm -rf /srv/www/config
 ln -sf \
   /var/lib/piwik/config \
-  /srv/www/config
+  ${CADDY_WEBROOT}/config
 
 echo "> symlink app archive"
 rm -rf /srv/www/archive
 ln -sf \
   /var/lib/piwik/archive \
-  /srv/www/archive
+  ${CADDY_WEBROOT}/archive
